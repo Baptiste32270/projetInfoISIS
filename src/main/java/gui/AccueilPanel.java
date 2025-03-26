bd3a6e0dc409597b0920c98064bfa941403a5b73
Baptiste32270 <baptiste.faugeres@sfr.fr>
2025-03-26 15:41:23 +0100

    Merge f2
    # Conflicts:
    #	src/main/java/com/mycompany/testgit/Testgit.java

--- src/main/java/gui/AccueilPanel.java
+++ src/main/java/gui/AccueilPanel.java
@@ -14,29 +14,28 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class AccueilPanel extends JPanel {
-	
 	public AccueilPanel(JFrame frame) {
-		this.setLayout(null);
-		Font font = new Font("Arial", Font.BOLD, 20);
+            this.setLayout(null);
+            Font font = new Font("Arial", Font.BOLD, 20);
                 
-		JButton dessin = new JButton("Ardoise Magique");
-		dessin.setBounds(607,210,300,100);
-		dessin.setFont(font);
-		this.add(dessin);
-                dessin.addActionListener(e -> {
-                    frame.setContentPane(new ArdoiseFacile(frame));
-                    frame.revalidate();
-                    frame.repaint();
-                });
+            JButton dessin = new JButton("Ardoise Magique");
+            dessin.setBounds(607,210,300,100);
+            dessin.setFont(font);
+            this.add(dessin);
+            dessin.addActionListener(e -> {
+                frame.setContentPane(new ArdoiseFacile(frame));
+                frame.revalidate();
+                frame.repaint();
+            });
                 
-		JButton pendu = new JButton("Pendu");
-		pendu.setBounds(607,330,300,100);
-		pendu.setFont(font);
-		this.add(pendu);
+            JButton pendu = new JButton("Pendu");
+            pendu.setBounds(607,330,300,100);
+            pendu.setFont(font);
+            this.add(pendu);
                 
-		JButton calcul = new JButton("Calcul Mental");
-		calcul.setBounds(607,450,300,100);
-		calcul.setFont(font);
-		this.add(calcul);
-	}
+            JButton calcul = new JButton("Calcul Mental");
+            calcul.setBounds(607,450,300,100);
+            calcul.setFont(font);
+            this.add(calcul);
+    }
 }
--- src/main/java/gui/MenuBar.java
+++ src/main/java/gui/MenuBar.java
@@ -17,54 +17,53 @@ import javax.swing.JSeparator;
 public class MenuBar extends JMenuBar {
 	private JFrame frame;
 	public MenuBar(JFrame frame) {
-                ArdoiseFacile ardoiseFacile = new ArdoiseFacile(frame);
-                ArdoiseDifficile ardoiseDifficile = new ArdoiseDifficile(frame);
+            ArdoiseFacile ardoiseFacile = new ArdoiseFacile(frame);
+            ArdoiseDifficile ardoiseDifficile = new ArdoiseDifficile(frame);
             
-                this.frame = frame;
-		JMenu activite = new JMenu("Activités");
-		JMenuItem dessin = new JMenuItem("Ardoise Magique");
-                dessin.addActionListener(e -> {
-                    frame.setContentPane(new ArdoiseFacile(frame));
+            this.frame = frame;
+            JMenu activite = new JMenu("Activités");
+            JMenuItem dessin = new JMenuItem("Ardoise Magique");
+            dessin.addActionListener(e -> {
+                frame.setContentPane(new ArdoiseFacile(frame));
+                frame.revalidate();
+                frame.repaint();
+            });
+            JMenuItem calcul = new JMenuItem("Calcul Mental");
+            JMenuItem pendu = new JMenuItem("Pendu");
+		
+            JMenu retour = new JMenu("Accueil");
+            JMenuItem accueil = new JMenuItem("Accueil");
+            accueil.addActionListener(e -> {
+                frame.setContentPane(new AccueilPanel(frame));
+                frame.revalidate();
+                frame.repaint();
+            });
+                
+            JMenu difficulte = new JMenu("Difficulté");
+            JMenuItem facile = new JMenuItem("Facile");
+            JMenuItem difficile = new JMenuItem("Difficile");
+            difficile.addActionListener(e -> {
+                if (frame.getContentPane() == ArdoiseFacile(frame)) {
+                    frame.setContentPane(new ArdoiseDifficile(frame));
                     frame.revalidate();
                     frame.repaint();
-                });
-		JMenuItem calcul = new JMenuItem("Calcul Mental");
-		JMenuItem pendu = new JMenuItem("Pendu");
+                }
+            });
 		
-                JMenu retour = new JMenu("Accueil");
-                JMenuItem accueil = new JMenuItem("Accueil");
-                accueil.addActionListener(e -> {
-                    frame.setContentPane(new AccueilPanel(frame));
-                    frame.revalidate();
-                    frame.repaint();
-                });
+            difficulte.add(facile);
+            difficulte.add(new JSeparator());
+            difficulte.add(difficile);
                 
-		JMenu difficulte = new JMenu("Difficulté");
-		JMenuItem facile = new JMenuItem("Facile");
-		JMenuItem difficile = new JMenuItem("Difficile");
-                difficile.addActionListener(e -> {
-                    if (frame.getContentPane() == ardoiseFacile) {
-                        frame.setContentPane(ardoiseDifficile);
-                        frame.revalidate();
-                        frame.repaint();
-                    }
-                });
+            retour.add(accueil);
 		
-		difficulte.add(facile);
-		difficulte.add(new JSeparator());
-		difficulte.add(difficile);
-                
-                retour.add(accueil);
+            activite.add(dessin);
+            activite.add(new JSeparator());
+            activite.add(calcul);
+            activite.add(new JSeparator());
+            activite.add(pendu);
 		
-		activite.add(dessin);
-		activite.add(new JSeparator());
-		activite.add(calcul);
-		activite.add(new JSeparator());
-		activite.add(pendu);
-		
-                this.add(retour);
-		this.add(activite);
-		this.add(difficulte);
+            this.add(retour);
+            this.add(activite);
+            this.add(difficulte);
 	}
-
 }
