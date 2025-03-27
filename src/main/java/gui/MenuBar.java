6189b9f1a294105dd7cde86d9ead7c4445ee1f9f
Baptiste32270 <baptiste.faugeres@sfr.fr>
2025-03-27 11:33:04 +0100

    Merge commit 'ac6fa8fd6c3fa97e004d0103cbd68bfabfda31fb'
    # Conflicts:
    #	src/main/java/gui/MenuBar.java

--- src/main/java/gui/MenuBar.java
+++ src/main/java/gui/MenuBar.java
@@ -10,6 +10,7 @@ package gui;
  */
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
